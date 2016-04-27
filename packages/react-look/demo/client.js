3b08ccb16a01b990c32c61114678f5017dfd5744
mattkrick <matt.krick@gmail.com>
2016-04-26 21:08:52 -0500

    broken when SSR from server build

--- packages/react-look/demo/client.js
+++ packages/react-look/demo/client.js
@@ -4,27 +4,8 @@ import { StyleSheet, LookRoot, Presets, Plugins } from '../modules'
 
 import App from './app.jsx'
 
-StyleSheet.addCSS({
-  '*': {
-    padding: 0,
-    margin: 0,
-    fontFamily: '"Lato", sans-serif',
-    fontWeight: 300,
-    boxSizing: 'border-box',
-    userSelect: 'none'
-  }
-})
-
 const config = Presets['react-dom']
 config.styleElementId = '_look'
-
-// config.plugins.push(Plugins.friendlyClassName)
-// config.plugins.push(Plugins.styleLogger)
-// config.styleLogger = {
-//  onEvent: 'onClick',
-//  onlyTopMost: true,
-//  noEmpty: true
-// }
 
 render(
   <LookRoot config={config}>
--- packages/react-look/demo/server.js
+++ packages/react-look/demo/server.js
@@ -4,7 +4,9 @@ import React from 'react'
 import { renderToString } from 'react-dom/server'
 import App from './app.jsx'
 import fs from 'fs'
-import { Presets, LookRoot, StyleSheet } from '../modules'
+import {Presets, LookRoot, StyleSheet} from '../modules'
+import {match, RouterContext} from 'react-router';
+import routes from '../build/prerender';
 
 const indexHTML = fs.readFileSync(__dirname + '/index.html').toString()
 const app = express()
@@ -16,19 +18,27 @@ app.use('/app.js', proxy('localhost:8080', {
 }))
 
 app.get('/', (req, res) => {
-  const serverConfig = Presets['react-dom'];
-  serverConfig.userAgent = req.headers['user-agent'];
 
-  const appHtml = renderToString(
-    <LookRoot config={serverConfig}>
-      <App />
-    </LookRoot>
-  )
-
-  const appCSS = StyleSheet.renderToString(serverConfig.prefixer)
-
-  res.write(indexHTML.replace('<!-- {{app}} -->', appHtml).replace('<!-- {{css}} -->', appCSS))
-  res.end()
+  // Note that req.url here should be the full URL path from
+  // the original request, including the query string.
+  match({routes, location: req.url}, (error, redirectLocation, renderProps) => {
+    if (error) {
+      res.status(500).send(error.message)
+    } else if (redirectLocation) {
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+    } else if (renderProps) {
+      // You can also check renderProps.components or renderProps.routes for
+      // your "not found" component or route respectively, and send a 404 as
+      // below, if you're using a catch-all route.
+      const serverConfig = Presets['react-dom'];
+      serverConfig.userAgent = req.headers['user-agent'];
+      const appCSS = StyleSheet.renderToString(serverConfig.prefixer);
+      const appHtml = renderToString(<LookRoot config={serverConfig}><RouterContext {...renderProps} /></LookRoot>);
+      res.send(indexHTML.replace('<!-- {{app}} -->', appHtml).replace('<!-- {{css}} -->', appCSS))
+    } else {
+      res.status(404).send('Not found')
+    }
+  })
 })
 
 app.listen(port, host, () => {
